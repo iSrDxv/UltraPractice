e33f25c0e2b5587240f4122a434a8d537b47558e
srclauyt@gmail.com <srclauyt@gmail.com>
2022-10-02 17:20:59 -0300

    added libraries

--- src/UltraPractice.php
+++ src/UltraPractice.php
@@ -5,13 +5,21 @@ namespace isrdxv\ultrapractice;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
 
+use JackMD\ConfigUpdater\ConfigUpdater;
+use JackMD\UpdateNotifier\UpdateNotifier;
+
 class UltraPractice extends PluginBase 
 {
   use SingletonTrait;
   
+  public const CONFIG_VERSION = 0;
+  
   protected function onLoad(): void
   {
-    $this->saveDefaultConfig();
+    UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
+    if (ConfigUpdater::checkUpdate($this, $this->getConfig(), "version", self::CONFIG_VERSION)) {
+      $this->reloadConfig();
+    }
   }
   
 }
